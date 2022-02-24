130ae5943d51b11edd25f773158bef6a57379214
liamcannon <liam.cannon@mymail.champlain.edu>
2022-02-24 15:10:07 -0500

    init unit tests

---
+++ server/sum.test.js
@@ -0,0 +1,8 @@
+// RUN WITH npm run test
+
+//EXAMPLE TEST
+//const sum = require('./sum');
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+//module.exports = sum;
