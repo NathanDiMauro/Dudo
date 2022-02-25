a756d8c6e293e0104f910bc5ecfb31cd6cdddfba
AdamCerutti <adamcerutti1996@gmail.com>
2022-02-25 11:07:59 -0500

    New About/License pages
    Fleshed out the "About" and "License" pages.
    Added a link to the footer which opens the GitHub page in a new tab.

--- client/dudo/src/pages/about.js
+++ client/dudo/src/pages/about.js
@@ -3,7 +3,22 @@ import React from 'react'
 class About extends React.Component {
     render() {
       return (
+        <><div style={{marginLeft: '25%',marginRight: '25%'}}>
         <h1>About</h1>
+        <p>Dudo is a liar's dice game. Learn more <a href="https://en.wikipedia.org/wiki/Dudo" target="_blank">HERE</a></p>
+        <p>We at ScrumShot strive to create an online hub for players to enjoy the wonderful game that is Dudo.</p><br></br>
+        <p>
+          <h4>Team Members</h4>
+          <ul><h5>Front-end</h5>
+            <li>Nathan DiMauro</li>
+            <li>Adam Cerutti</li>
+          </ul>
+          <ul><h5>Back-end</h5>
+            <li>Liam Cannon</li>
+            <li>Jake Capra</li>
+          </ul>
+        </p>
+        </div></>
       )}
   }
   export default About
