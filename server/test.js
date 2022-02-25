351cb0cfda00d7b54c2b79282d4eda81a3ee4b99
liamcannon <liam.cannon@mymail.champlain.edu>
2022-02-25 02:06:45 -0500

    test example / first test? kinda | im having trouble figuring this out

---
+++ server/test.js
@@ -0,0 +1,21 @@
+// RUN WITH npm test
+
+//EXAMPLE TEST
+//const sum = require('./sum');
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+//module.exports = sum;
+// testing generateDice
+const Player = require("./players");
+
+describe("Player", () => {
+  const player = new player();
+  test("defines setRule()", () => {
+    expect(typeof player.setRule).toBe("function");
+  });
+});
+
+// I have no fucking clue how to do this rn 
+
+
