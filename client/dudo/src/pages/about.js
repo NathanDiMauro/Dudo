7fea71beedeb7328311b528537c04c933836e12c
AdamCerutti <adamcerutti1996@gmail.com>
2022-02-25 11:40:26 -0500

    Styling
    Added proper styling to about.js and license.js

--- client/dudo/src/pages/about.js
+++ client/dudo/src/pages/about.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import '../styles/about.css'
 
 class About extends React.Component {
     render() {
       return (
-        <><div style={{marginLeft: '25%',marginRight: '25%'}}>
+        <div id='about'>
         <h1>About</h1>
-        <p>Dudo is a liar's dice game. Learn more <a href="https://en.wikipedia.org/wiki/Dudo" target="_blank">HERE</a></p>
+        <p>Dudo is a liar's dice game. Learn more <a href="https://en.wikipedia.org/wiki/Dudo" target="new">HERE</a></p>
         <p>We at ScrumShot strive to create an online hub for players to enjoy the wonderful game that is Dudo.</p><br></br>
-        <p>
+        <div>
           <h4>Team Members</h4>
           <ul><h5>Front-end</h5>
             <li>Nathan DiMauro</li>
@@ -17,8 +18,8 @@ class About extends React.Component {
             <li>Liam Cannon</li>
             <li>Jake Capra</li>
           </ul>
-        </p>
-        </div></>
+        </div>
+        </div>
       )}
   }
   export default About
