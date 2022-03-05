0d823788897578e53df61ffb39495e10d5c04b34
Nathan DiMauro <nathandimauro1@gmail.com>
2022-03-05 12:29:44 -0500

    bid styling changes

--- client/dudo/src/components/playerActions.js
+++ client/dudo/src/components/playerActions.js
@@ -11,7 +11,7 @@ import ReactTooltip from 'react-tooltip';
 
 const PlayerActions = (props) => {
     
-    const [chosenDice, setChosenDice] = useState(null);
+    const [chosenDice, setChosenDice] = useState(dice1);
 
     const [action, setAction] = useState(null);
     const [amount, setAmount] = useState(null);
@@ -69,12 +69,21 @@ const PlayerActions = (props) => {
         document.getElementById("diceDropdown").style.display = "none";
     }
 
+    function hideDiceDropDown(dice, diceNum) {
+        document.getElementById("diceSelected").style.display = "inline-block";
+
+        // Hide dropdown
+        document.getElementById("diceDropdown").style.display = "none";
+    }
+
+
+
     return (
         <div id="playerAction">
             <input id='amountInput' />
             
             <button id='dropdownSelect' onClick={showDiceDropdown}>Select Dice</button>
-            <div id='diceDropdown'>
+            <div id='diceDropdown' onMouseLeave={hideDiceDropDown}>
                 <img src={dice1} alt='Dice one' onClick={() => selectDice(dice1, 1)} />
                 <img src={dice2} alt='Dice two' onClick={() => selectDice(dice2, 2)} />
                 <img src={dice3} alt='Dice three' onClick={() => selectDice(dice3, 3)} />
@@ -82,11 +91,14 @@ const PlayerActions = (props) => {
                 <img src={dice5} alt='Dice five' onClick={() => selectDice(dice5, 5)} />
                 <img src={dice6} alt='Dice six' onClick={() => selectDice(dice6, 6)} />
             </div>
-            <ReactTooltip place="bottom" type="dark" effect="solid"/>
-            <img id='diceSelected' onClick={showDiceDropdown} alt='Dice selected' src={chosenDice} data-tip="Change Dice"/>
-            <button id='betButton' onClick={() => setAction('raise')}>Raise</button>
-            <button id='callButton' onClick={() => setAction('call')}>Call</button>
-            <button id='spotButton' onClick={() => setAction('spot')}>Spot</button>
+            <ReactTooltip place="right" type="dark" effect="solid"/>
+            <img id='diceSelected' onMouseOver={showDiceDropdown} alt='Dice selected' src={chosenDice} data-tip="Change Dice"/>
+            <br />
+            <div id="bidButtons">
+                <button id='betButton' onClick={() => setAction('raise')}>Raise</button>
+                <button id='callButton' onClick={() => setAction('call')}>Call</button>
+                <button id='spotButton' onClick={() => setAction('spot')}>Spot</button>
+            </div>
         </div>
     );
 }
