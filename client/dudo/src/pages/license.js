e03a2a96d11c15e0ccc3ec7b4d58445679bc39ed
AdamCerutti <adamcerutti1996@gmail.com>
2022-03-10 22:43:26 -0500

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/JakeCapra/Scrumshot"""
    This reverts commit d165ed45e5133d8ac93b2971b6fcbdd9fac0d3bd.

--- client/dudo/src/pages/license.js
+++ client/dudo/src/pages/license.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import '../styles/license.css'
 
 class License extends React.Component {
     render() {
       return (
-      <div id='license'>
-        <h1>MIT License</h1>
-        <p>Copyright (c) 2022 Liam Cannon, Nathan DiMauro, Adam Cerutti, Jake Capra</p><br></br>
-        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
-        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
-        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
-        <a href="https://github.com/JakeCapra/Scrumshot/blob/main/LICENSE.md?plain=1" target="_blank">License on GitHub</a>
-      </div>
+        <h1>License</h1>
       )}
   }
   export default License
