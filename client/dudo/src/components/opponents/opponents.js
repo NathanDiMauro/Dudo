20257657f6c871de9f485a522ad81810bfc1c7a6
Jake Capra <jacob.capra@mymail.champlain.edu>
2022-05-05 14:37:03 -0400

    one way to do a circle

--- client/dudo/src/components/opponents/opponents.js
+++ client/dudo/src/components/opponents/opponents.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import Opponent from './opponent';
 import { SocketContext } from '../../context/socketContext';
 import '../../styles/opponent.css'
@@ -7,11 +7,22 @@ import '../../styles/opponent.css'
 const Opponents = () => {
     // const { players } = useContext(SocketContext);
     // const players = [{playerName: "Tim", diceCount: 5}, {playerName: "Tim", diceCount: 5}]
-    const players = [{playerName: "Tim", dice: [2, 2], diceCount: 5}, {playerName: "Tim", dice: [2, 2, 2, 2, 2], diceCount: 5}]
+    // const players = [{ playerName: "Tim", dice: [2, 2], diceCount: 5 }, { playerName: "Tim", dice: [2, 2, 2, 2, 2], diceCount: 5 }, { playerName: "Tim", dice: [2, 2, 2, 2, 2], diceCount: 5 }]
+    const players = [{ playerName: "1", diceCount: 5 }, { playerName: "2", diceCount: 5 }, { playerName: "3",  diceCount: 5 }, { playerName: "4",  diceCount: 5 }]
+
+    useEffect(() => {
+            const el = document.querySelector('.opponents ul');
+            Array.from(el.children).forEach((li, idx) => {
+                const rotation = idx * 360 / players.length;
+                li.style.transform = `translate(-50%, -50%) rotate(${rotation}deg) translateY(8rem) rotate(-${rotation}deg)`
+            })
+    }, [players])
 
     return (
         <div className='opponents'>
-            {players.map((player, key) => <Opponent key={key} name={player.playerName} diceCount={player?.diceCount} dice={player?.dice} />)}
+            <ul>
+                {players.map((player, key) => <li key={key}><Opponent name={player.playerName} diceCount={player?.diceCount} dice={player?.dice} /></li>)}
+            </ul>
         </div>
     )
 }
