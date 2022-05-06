efe18976661a1b9da1e82b8530dd72ef8e3ff416
Jake Capra <jacob.capra@mymail.champlain.edu>
2022-05-06 15:47:21 -0400

    reverted circle players

--- client/dudo/src/components/opponents/opponents.js
+++ client/dudo/src/components/opponents/opponents.js
@@ -5,24 +5,20 @@ import '../../styles/opponent.css'
 
 
 const Opponents = () => {
-    // const { players } = useContext(SocketContext);
-    // const players = [{playerName: "Tim", diceCount: 5}, {playerName: "Tim", diceCount: 5}]
-    // const players = [{ playerName: "Tim", dice: [2, 2], diceCount: 5 }, { playerName: "Tim", dice: [2, 2, 2, 2, 2], diceCount: 5 }, { playerName: "Tim", dice: [2, 2, 2, 2, 2], diceCount: 5 }]
-    const players = [{ playerName: "1", diceCount: 5 }, { playerName: "2", diceCount: 5 }, { playerName: "3",  diceCount: 5 }, { playerName: "4",  diceCount: 5 }]
+    const { players } = useContext(SocketContext);
 
-    useEffect(() => {
-            const el = document.querySelector('.opponents ul');
-            Array.from(el.children).forEach((li, idx) => {
-                const rotation = idx * 360 / players.length;
-                li.style.transform = `translate(-50%, -50%) rotate(${rotation}deg) translateY(8rem) rotate(-${rotation}deg)`
-            })
-    }, [players])
+    // Arrange opponents in circle - see commit history for how to implement
+    // useEffect(() => {
+    //         const el = document.querySelector('.opponentsCircle ul');
+    //         Array.from(el.children).forEach((li, idx) => {
+    //             const rotation = idx * 360 / players.length;
+    //             li.style.transform = `translate(-50%, -50%) rotate(${rotation}deg) translateY(8rem) rotate(-${rotation}deg)`
+    //         })
+    // }, [players])
 
     return (
         <div className='opponents'>
-            <ul>
-                {players.map((player, key) => <li key={key}><Opponent name={player.playerName} diceCount={player?.diceCount} dice={player?.dice} /></li>)}
-            </ul>
+                {players.map((player, key) => <Opponent key={key} name={player.playerName} diceCount={player?.diceCount} dice={player?.dice} />)}
         </div>
     )
 }
