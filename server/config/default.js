e30556734f499a3e49aac2931fa2d9daf3d8ee22
Jake Capra <jacob.capra@mymail.champlain.edu>
2022-06-16 12:44:26 -0400

    docker stuff

--- server/config/default.js
+++ server/config/default.js
@@ -1,7 +1,7 @@
 const defaultConfig = {
     corsOrigin: ['http://localhost:3000'],
     methods: ['GET', 'POST'],
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 5000,
     methods: ['GET', 'POST']
 }
